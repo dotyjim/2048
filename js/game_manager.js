161e67b4632e5338fabe7763d8054bfe577c17be
Ranjay Krishna <rak248@cornell.edu>
2014-12-07 12:57:00 -0800

    adding checks for inputs on constructor

--- js/game_manager.js
+++ js/game_manager.js
@@ -1,29 +1,37 @@
 function GameManager(size, InputManager, Actuator, StorageManager) {
   this.size           = size; // Size of the grid
-  this.inputManager   = new InputManager;
-  this.storageManager = new StorageManager;
-  this.actuator       = new Actuator;
+  if (InputManager)
+    this.inputManager   = new InputManager;
+  if (StorageManager)
+    this.storageManager = new StorageManager;
+  if (Actuator)
+    this.actuator       = new Actuator;
 
   this.startTiles     = 2;
 
-  //this.inputManager.on("move", this.move.bind(this));
-  this.inputManager.on("restart", this.restart.bind(this));
-  this.inputManager.on("keepPlaying", this.keepPlaying.bind(this));
+  if (this.inpurManager) {
+    //this.inputManager.on("move", this.move.bind(this));
+    this.inputManager.on("restart", this.restart.bind(this));
+    this.inputManager.on("keepPlaying", this.keepPlaying.bind(this));
+  }
 
   this.setup();
 }
 
 // Restart the game
 GameManager.prototype.restart = function () {
-  this.storageManager.clearGameState();
-  this.actuator.continueGame(); // Clear the game won/lost message
+  if (this.storageManager) 
+    this.storageManager.clearGameState();
+  if (this.actuator)
+    this.actuator.continueGame(); // Clear the game won/lost message
   this.setup();
 };
 
 // Keep playing after winning (allows going over 2048)
 GameManager.prototype.keepPlaying = function () {
   this.keepPlaying = true;
-  this.actuator.continueGame(); // Clear the game won/lost message
+  if (this.actuator)
+    this.actuator.continueGame(); // Clear the game won/lost message
 };
 
 // Return true if the game is lost, or has won and the user hasn't kept playing
@@ -33,29 +41,18 @@ GameManager.prototype.isGameTerminated = function () {
 
 // Set up the game
 GameManager.prototype.setup = function () {
-  //var previousState = this.storageManager.getGameState();
+  this.grid        = new Grid(this.size);
+  this.score       = 0;
+  this.over        = false;
+  this.won         = false;
+  this.keepPlaying = false;
 
-  // Reload the game from a previous game if present
-  /*if (previousState) {
-    this.grid        = new Grid(previousState.grid.size,
-                                previousState.grid.cells); // Reload grid
-    this.score       = previousState.score;
-    this.over        = previousState.over;
-    this.won         = previousState.won;
-    this.keepPlaying = previousState.keepPlaying;
-  } else {*/
-    this.grid        = new Grid(this.size);
-    this.score       = 0;
-    this.over        = false;
-    this.won         = false;
-    this.keepPlaying = false;
-
-    // Add the initial tiles
-    this.addStartTiles();
-  //}
+  // Add the initial tiles
+  this.addStartTiles();
 
   // Update the actuator
-  this.actuate();
+  if (this.storageManager && this.actuator)
+    this.actuate();
 };
 
 // Set up the initial tiles to start the game with
